ca8c0ac511f2f85d31e6eda1e793911f0aea490f
Freddy Drennan <drennanfreddy@gmail.com>
2020-12-21 17:17:49 -0700

    updtate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::env;
 
 fn main() -> std::io::Result<()> {
-    let path = env::current_dir()?;
-    println!("The current directory is {}", path.display());
+    let dir = env::current_dir();
+
+
+    println!("The current directory is {:?}", dir);
+
     Ok(())
 }
